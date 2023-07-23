34b9ef0451de23a762140bb3180bb817d818449a
priya <20ee074@gpriet.ac.in>
2023-07-23 14:12:34 +0530

    UserId Cls

--- src/main/java/model/UserID.java
+++ src/main/java/model/UserID.java
@@ -1,11 +1,16 @@
 package model;
 
 public class UserID {
-    private String username;
-    private String email;
-    private String Password;
-    private String First_Name;
-    private String Last_Name;
-    private int Phone_number;
-    private String Address;
+    public static <args> void main(String[] args) {
+
+
+        private String username;
+        private String email;
+        private String Password;
+        private String First_Name;
+        private String Last_Name;
+        private int Phone_number;
+        private String Address;
+    }
+
 }
