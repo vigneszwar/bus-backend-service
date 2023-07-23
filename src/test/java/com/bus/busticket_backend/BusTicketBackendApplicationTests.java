53b840eb8b5a38b09b6cff865e5dd9ee625e0da1
Vishnu <vishnuakarikalan@gmail.com>
2023-07-23 10:49:56 +0530

    change

---
+++ src/test/java/com/bus/busticket_backend/BusTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bus.busticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
