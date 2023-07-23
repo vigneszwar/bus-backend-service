475bf46b7917467c3a03ce1f6935121ce64f001c
priya <20ee074@gpriet.ac.in>
2023-07-23 14:05:23 +0530

    UserId Cls

---
+++ src/main/java/model/UserID.java
@@ -0,0 +1,11 @@
+package model;
+
+public class UserID {
+    private String username;
+    private String email;
+    private String Password;
+    private String First_Name;
+    private String Last_Name;
+    private int Phone_number;
+    private String Address;
+}
